98fdc0974cfd9a88ad2dfaa3c6369a9e93fb8b11
Indhu <43375525+indhusundar@users.noreply.github.com>
2023-02-13 14:06:58 +0800

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,7 @@ provider "aws"{
 resource "aws_instance" "AWSInstance"{
     ami = "ami-0319ac76374b9fe74"
 	instance_type = "t2.micro"
-	key_name = "indhu"
+	key_name = "Indhu"
 	security_groups = ["launch-wizard-2"]
 	tags = {
 	 Name = "tomcatservers"
