2087d2776f53e637e0bd341c02a341bcc4e6d389
Indhu <43375525+indhusundar@users.noreply.github.com>
2023-02-13 14:04:20 +0800

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 
 resource "aws_instance" "AWSInstance"{
-    ami = "ami-08cd0547bf85331e7"
+    ami = "ami-0319ac76374b9fe74"
 	instance_type = "t2.micro"
 	key_name = "indhu"
 	security_groups = ["launch-wizard-2"]
